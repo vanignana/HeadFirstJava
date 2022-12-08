2949ddefc05d3cef89dafaedbd69fe419fff6e55
vanignana <vanisankararaj@gmail.com>
2022-12-08 16:11:04 -0500

    Initial Commit

--- src/main/java/object/EchoTestDrive.java
+++ src/main/java/object/EchoTestDrive.java
@@ -3,7 +3,8 @@ package object;
 public class EchoTestDrive {
     public static void main(String[] args) {
         Echo e1 = new Echo();
-        Echo e2 = new Echo();//
+       // Echo e2 = new Echo();//
+        Echo e2 = e1;
         int x = 0; //
 
         while (x < 4) {
