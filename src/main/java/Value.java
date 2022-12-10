5607a3495ca04c0e5a10c8c30a22cd92a6ed6940
vanignana <vanisankararaj@gmail.com>
2022-12-09 21:00:47 -0500

    Initial Commit

---
+++ src/main/java/Value.java
@@ -0,0 +1,16 @@
+public class Value {
+    int intValue;
+
+    public int doStuff(int factor){
+        if (intValue > 100) {
+            return intValue * factor;
+        }
+        else
+        {
+            return intValue * (5 - factor);
+        }
+    }
+
+
+}
+
