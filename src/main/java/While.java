095bb40ef438b2e7403681a0e018888531f91bc8
vanignana <vanisankararaj@gmail.com>
2022-12-16 10:19:38 -0500

    Initial Commit

---
+++ src/main/java/While.java
@@ -0,0 +1,9 @@
+public class While {
+    public static void main(String[] args) {
+        int i = 0;
+        while( i < 8){
+            System.out.println("I is : " +i);
+            i++;
+        }
+    }
+}
